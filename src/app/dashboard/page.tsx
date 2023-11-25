b666575a170b25da5165dbc1466c93db3024e5af
North Hess <north4h@gmail.com>
2023-11-25 11:14:00 -0500

    figuring out imports

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Stickynote } from "./stickynote"
+
+export default function Dashboard() {
+    return (
+        <main>
+            <h1>This is the dashboard page</h1>
+            <Stickynote />
+        </main>
+    )
+}
---
+++ src/app/dashboard/stickynote.tsx
@@ -0,0 +1,8 @@
+
+export function Stickynote() {
+   return (
+        <div className="text-red-700">
+            <h1>hello from stickynote</h1>
+        </div>
+   )    
+}
