e4d4a103c8084d99544c7d06b0736512e164d83a
North Hess <north4h@gmail.com>
2024-01-18 18:16:20 -0500

    Editable sticky notes
    Sticky notes can have their title and content modified. Does not save to database yet

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
-import { Stickynote } from "./stickynote"
+import { Stickynote } from "./stickynote";
 
 export default function Dashboard() {
-    return (
-        <main>
-            <h1>This is the dashboard page</h1>
-            <Stickynote />
-        </main>
-    )
+  return (
+    <main>
+      <h1>This is the dashboard page</h1>
+      <div className="flex flex-row flex-wrap w-full justify-center gap-8">
+        <Stickynote />
+        <Stickynote />
+        <Stickynote />
+      </div>
+    </main>
+  );
 }
--- src/app/dashboard/stickynote.tsx
+++ src/app/dashboard/stickynote.tsx
@@ -1,8 +1,40 @@
+"use client";
+import { SetStateAction, useState } from "react";
 
 export function Stickynote() {
-   return (
-        <div className="text-red-700">
-            <h1>hello from stickynote</h1>
-        </div>
-   )    
+  const [title, setTitle] = useState("Title");
+  const [content, setContent] = useState("Example content");
+  const [time, setTime] = useState("dummy time");
+
+  const changeTitle = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setTitle(event.target.value);
+  };
+
+  const changeContent = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setContent(event.target.value);
+  };
+
+  return (
+    <div className="border-2 border-black rounded-sm bg-yellow-200 shadow-xl flex flex-col w-1/4">
+      <form>
+        <textarea
+          maxLength={42}
+          value={title}
+          onChange={changeTitle}
+          className="bg-yellow-300 w-full text-xl h-10 px-2 pt-1.5 resize-none outline-none"
+        ></textarea>
+        <textarea
+          maxLength={512}
+          value={content}
+          onChange={changeContent}
+          className="bg-inherit text-md h-56 px-2 w-full resize-none outline-none"
+        ></textarea>
+        <h2 className="text-xs italic px-2">Date of sticky note</h2>
+      </form>
+    </div>
+  );
 }
