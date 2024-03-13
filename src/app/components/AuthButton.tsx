46c54ac811631a7c913e6b14a2403e310ca84c83
North Hess <north4h@gmail.com>
2024-03-13 17:13:29 -0400

    fix: sign in button styling fixed missing light/dark mode variants for sign in button

--- src/app/components/AuthButton.tsx
+++ src/app/components/AuthButton.tsx
@@ -19,9 +19,9 @@ export function AuthButton({ session }: SessionProp) {
   }
   return (
     <div className="flex flex-col justify-center">
-      <div className="flex h-8 flex-col justify-center rounded bg-gray-700 ring-2 ring-gray-700">
+      <div className="flex h-8 flex-col justify-center rounded bg-gray-200 ring-2 ring-gray-200 dark:bg-gray-700 dark:ring-gray-700">
         <button
-          className="mx-2 h-6 font-mono text-base tracking-tight transition-colors duration-300 ease-in-out hover:text-gray-300"
+          className="mx-2 h-6 font-mono text-base tracking-tight text-black transition-colors duration-300 ease-in-out hover:text-gray-500 dark:text-white dark:hover:text-gray-300"
           onClick={() => signIn()}
         >
           Sign In
