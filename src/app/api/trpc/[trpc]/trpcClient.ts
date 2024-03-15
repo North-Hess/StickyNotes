1104b4015835d970270fe2c511fd8c637b7d070f
North Hess <north4h@gmail.com>
2024-03-14 20:47:44 -0400

    loggin variable

--- src/app/api/trpc/[trpc]/trpcClient.ts
+++ src/app/api/trpc/[trpc]/trpcClient.ts
@@ -3,6 +3,7 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { transformer } from "@/app/server/api/transformer";
 
 const getURL = () => {
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
   if (process.env.NEXT_PUBLIC_VERCEL_URL)
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
