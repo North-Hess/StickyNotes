b1e8334af3239eba856a9e45029d707aef7cd5f9
North Hess <north4h@gmail.com>
2024-03-14 20:43:18 -0400

    fix: correcting environment variable name

--- src/app/api/trpc/[trpc]/trpcClient.ts
+++ src/app/api/trpc/[trpc]/trpcClient.ts
@@ -3,7 +3,7 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { transformer } from "@/app/server/api/transformer";
 
 const getURL = () => {
-  if (process.env.VERCEL_URL)
+  if (process.env.NEXT_PUBLIC_VERCEL_URL)
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 };
