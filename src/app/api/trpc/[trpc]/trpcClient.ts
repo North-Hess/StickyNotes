710e2ba84eaec973f9329d3263f5527b8a86132e
North Hess <north4h@gmail.com>
2024-03-14 20:39:35 -0400

    fix: switched trpcClient variable changed accessing VERCEL_URL to NEXT_PUBLIC_VERCEL_URL

--- src/app/api/trpc/[trpc]/trpcClient.ts
+++ src/app/api/trpc/[trpc]/trpcClient.ts
@@ -3,7 +3,8 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { transformer } from "@/app/server/api/transformer";
 
 const getURL = () => {
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.VERCEL_URL)
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 };
 
