222fce40340a940ec2cdc5a88cd211bdbe246275
North Hess <north4h@gmail.com>
2024-03-16 19:29:06 -0400

    added loading page for notes also added some Suspense components to that seem to speed up some loading times

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,24 +1,26 @@
-import { getAuth } from "../api/auth/[...nextauth]/getAuth";
+// import { getAuth } from "../api/auth/[...nextauth]/getAuth";
 import { AuthButton } from "./AuthButton";
 import { ProfilePicture } from "./ProfilePicture";
 import NavButton from "./NavButton";
 import { ThemeToggle } from "./ThemeToggle";
+import { Suspense } from "react";
 
 export default async function NavBar() {
-  const session = await getAuth();
   return (
     <div className="flex h-16 flex-row justify-between border-b border-gray-700 p-2 text-white first:pl-8">
       <nav className="flex flex-row gap-x-10">
         <NavButton text="Home" redirect="/" />
         <NavButton text="Dashboard" redirect="/dashboard" />
-        <AuthButton session={session} />
+        <AuthButton />
       </nav>
       <div className="flex flex-row">
         <div className="flex flex-col justify-center">
           <ThemeToggle />
         </div>
         <div className="pr-4">
-          <ProfilePicture session={session} />
+          <Suspense>
+            <ProfilePicture />
+          </Suspense>
         </div>
       </div>
     </div>
